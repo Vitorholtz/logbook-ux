7550d6357f0390262215f440e6a6ac8929d1f092
Vítor Holtz <vitorholtz27@gmail.com>
2021-02-24 12:10:19 -0300

    14

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ personasBtn.addEventListener ('click', () => {
     iconDown3.classList.toggle('fas-active');
 })
 
-// Ideaçaõ
+// Ideação
 
 let ideacaoBtn = document.querySelector('.ideate-toggle');
 let ideacaoSection = document.querySelector('.ideacao');
@@ -132,3 +132,20 @@ hamburgerToggle.addEventListener('click', () => {
     mobileMenu.classList.toggle('mobile-menu-active');
 
 })
+
+// to-the-top icon
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    let position = 1082;
+
+    if (currentScrollPos >= position) {
+        document.getElementById("stickhome_img").style.opacity = 1;
+        document.getElementById("stickhome_img").style.visibility = 'visible';
+        document.getElementById("stickhome_img").style.top = '50%';
+    } else {
+        document.getElementById("stickhome_img").style.opacity = 0;
+        document.getElementById("stickhome_img").style.visibility = 'hidden';
+        document.getElementById("stickhome_img").style.top = '0em';
+    }
+}
